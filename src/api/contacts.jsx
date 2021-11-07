3262bc7cfe8d43bc68946886353ef9d8a793358c
Triwichian <korrakot.triwichian@accenture.com>
2021-11-08 03:14:05 +0700

    updated

--- src/api/contacts.jsx
+++ src/api/contacts.jsx
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:3006",
-  baseURL: "https://samsantech-app.herokuapp.com/contacts",
+  baseURL: "https://samsantech-app.herokuapp.com/api",
 });
